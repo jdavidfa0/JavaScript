2ce90f59ba112031b5e2a7550cdd017615a0e228
jdavidfa0 <jdavidfa0@gmail.com>
2024-05-09 09:48:50 -0500

    Listado Gastos 3.0

--- Boostrap/js/funcional.js
+++ Boostrap/js/funcional.js
@@ -39,6 +39,49 @@ class UI
         document.querySelector('#restante').textContent = cantidad.restante;
 
     }
+    imprimiralerta(mensaje,tipo){
+        const divmensaje = document.createElement('div');
+        divmensaje.classList.add('text-center', 'alert');
+
+        if( tipo=='error'){
+            divmensaje.classList.add('alert-danger');
+    
+        }else{
+            divmensaje.classList.add('alert-sucess');
+        } 
+
+        divmensaje.textContent =mensaje;
+
+
+        document.querySelector('.contenido-gastos').insertBefore(divmensaje, formulario);
+
+        setTimeout(
+            () =>{
+                document.querySelector('.contenido-gastos .alert').remove();
+
+            },3000);
+
+
+    }
+    agregarlista(gastos){
+        this.limpiarHTML();
+        gastos.forEach(gasto=>{ const {nombre,cantidad,id}=gasto; 
+        const nuevogasto =document.createElement('li');
+        nuevogasto.className ='list-group-item d-flex justify-content-between aling-items-center'
+        nuevogasto.dataset.id=id;
+
+        nuevogasto.innerHTML = `${nombre} 
+        <span class="badge badge-primary badge-pill">$ ${cantidad} </span>`
+        
+        const btnborrar =document.createElement('button');
+        btnborrar.classList.add('btn', 'btn-danger', 'borrar-gasto');
+        nuevogasto.appendChild(btnborrar);
+
+        listgasto.appendChild(nuevogasto);
+
+    })
+
+    }
    
 
 
@@ -58,7 +101,26 @@ function preguntar(){
 
 }
 
-function agregar(){
+function agregar(e){
+    e.preventDefault()
+
+    const nombre = document.querySelector('#gasto').value;
+    const cantidad = Number(document.querySelector ('#cantidad').value);
+
+    if (nombre ==='' || cantidad === ''){
+        ui.imprimiralerta('Ambos campos son obligatorios', 'error');
+    }else{
+        const gasto= {nombre,cantidad,id:Date.now()}
+
+        Presupuesto.nuevogasto(gasto);
+
+        ui.imprimiralerta('Correcto', 'correcto');
+
+        const {gastos}= Presupuesto;
+        ui.agregarlista(gastos)
+    }
+
+
 
 }
 function eliminar(){
